64df75004334abe4bc021da4bc4d5c1767423adb
DeuterGraves <puckles@gmail.com>
2018-09-27 14:45:10 +0100

    actor basics set up

--- models/actor.rb
+++ models/actor.rb
@@ -2,9 +2,13 @@ require_relative("../db/sql_runner.rb")
 
 class Actor
 
+attr_accessor(:f_name, :l_name)
+attr_reader(:id)
 
-  def initialize()
-
+  def initialize(options)
+    @id = options["id"].to_i()
+    @f_name = options["f_name"]
+    @l_name = options["l_name"]
   end
 
 

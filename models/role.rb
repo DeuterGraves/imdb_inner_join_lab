411de1ed98bb6cc5ea3da9afbee0c361a985d8ab
DeuterGraves <puckles@gmail.com>
2018-09-27 14:47:47 +0100

    role basics set up

--- models/role.rb
+++ models/role.rb
@@ -1,3 +1,14 @@
 class Role
 
+def initialize(options)
+  @id = options["id"].to_i()
+  @movie_id = options["movie_id"].to_i()
+  @actor_id = options["actor_id"].to_i()
+  @fee = options["fee"] 
+end
+
+
+
+
+#class end
 end
